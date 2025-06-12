f8028c1a7e2fa5bef2e8071ddf2875b6db22f636
Greg Delgado <grd888@gmail.com>
2025-06-12 11:14:03 +0800

    include prompt as cli parameter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,9 +7,16 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+
+args = sys.argv
+if len(args) < 2:
+  print("Usage: python main.py \"Write your prompt here...\"")
+  exit(1)
+
+prompt = args[1]
 response = client.models.generate_content(
   model="gemini-2.0-flash-001", 
-  contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+  contents=prompt
 )
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
